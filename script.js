2cfdf5f224a80427c71aa2e5295a8acf15e829e6
DevitoDbug <Davidochiengy@gmail.com>
2023-04-13 16:54:01 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const DOG_URL ="https://dog.ceo/api/breeds/image/random"
+
+const doggos = document.querySelector(".image");
+console.log(doggos);
+
+function addNewDoggo(){
+    const promise  = fetch(DOG_URL);
+    promise.then(function(respose){
+        const processingPromise = respose.text();
+        return processingPromise;
+    })
+    .then(function(processedResponse){
+        const dogObject = JSON.parse(processedResponse);
+        const img = document.createElement("img");
+        img.src = dogObject.message;
+        img.alt = "Cute doggo";
+        img.className = "gen-image";
+        doggos.innerHTML = img.outerHTML;
+    });
+}
+
+document.querySelector("button").addEventListener("click",addNewDoggo);
